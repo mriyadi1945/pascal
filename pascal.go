3c3152f3ffde00281b7ed08dbe10b50f316b1901
Muhamad Riyadi <riyadi.proguru@gmail.com>
2026-02-15 16:58:54 +0700

    update pascal

--- pascal.go
+++ pascal.go
@@ -1,8 +1,22 @@
 package pascal
 
+import "fmt"
+
 func Pascal(col, row int) int {
 	if col == 0 || col == row {
 		return 1
 	}
 	return Pascal(col-1, row-1) + Pascal(col, row-1)
+}
+
+func PascalIterative(col, row int) {
+	for i := range 15 {
+		for j := 0; j <= 15-i; j++ {
+			fmt.Print("  ")
+		}
+		for k := 0; k < i; k++ {
+			fmt.Printf("%4d", Pascal(k, i-1))
+		}
+		fmt.Println()
+	}
 }
